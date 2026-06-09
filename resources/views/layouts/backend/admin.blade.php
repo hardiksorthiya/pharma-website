75125831f4d328024c1ef3ca8a9886e60f33eb55
Your New Name <your.new.email@example.com>
2026-06-09 11:30:19 +0530

    admin panel strcture with bootstrap

--- resources/views/layouts/backend/admin.blade.php
+++ resources/views/layouts/backend/admin.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'Dashboard') — {{ config('app.name', 'Sanskruti Pharma') }}</title>
+    <title>@yield('title', 'Dashboard')</title>
 
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/backend/style.css') }}">
